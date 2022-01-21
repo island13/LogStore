4a30ed42cf4a7d6556aa1ac0b31a2beff283c1dc
Andreas Svensson <svenssa@audible.com>
2022-01-21 10:52:44 -0800

    Initial Commit

---
+++ LogStore WatchKit Extension/LogStoreApp.swift
@@ -0,0 +1,19 @@
+//
+//  LogStoreApp.swift
+//  LogStore WatchKit Extension
+//
+//  Created by Svensson, Andreas on 1/21/22.
+//
+
+import SwiftUI
+
+@main
+struct LogStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
